d076d013b002f7f9a15314be0e566337728f19f2
Srinivas Krishnamurthy <ksrinivas0575@gmail.com>
2021-04-10 11:42:05 +0530

    committing my files in pycharm in windows

--- first.py
+++ first.py
@@ -1,2 +1,32 @@
-print("Hello world")
-print
+'''
+this is my first program
+written using pycharm
+author - Srinivas
+Date - 10th Apr
+contact - srinivas.k@ltts.com
+'''
+
+def my_func1():
+    print("this is my first function")
+
+my_func1()
+
+#i added one error
+
+
+'''
+made some error on 11th Apr 2021
+file crashed
+not getting the ouput
+
+10th apr 2021
+compare the changes
+
+100's lines
+
+'''
+
+def func_unknown():
+    pass
+
+func_unknown()
